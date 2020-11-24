0c1b760025a207a737846cfaf565e794239517ea
Hai <2000caothehai@gmail.com>
2020-11-24 17:06:58 +0700

    Bai tap git state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactLayout from './Components/ReactLayout';
-import SideNav from './Components/SideNav';
+import './css/style.css'
 import Main from './Components/Main';
-import './css/stylemain.css';
-import Footer from './Components/Footer';
+
 function App() {
   return (
     <div className="App">
-      <ReactLayout></ReactLayout>
-      <SideNav></SideNav>
-      <Main></Main>
-      <Footer></Footer>
+ <Main></Main>
     </div>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,72 +1,108 @@
 import React, { Component } from 'react'
 
-
 export default class Main extends Component {
-    items = [
-        { title: 'Item one', price: '$24.99', description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veritatis vitae sapiente at assumenda ipsum. Et, deleniti nostrum accusamus commodi alias quaerat distinctio dolorum labore, magnam libero quam. Illo, quo.' },
-        { title: 'Item one', price: '$24.99', description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veritatis vitae sapiente at assumenda ipsum. Et, deleniti nostrum accusamus commodi alias quaerat distinctio dolorum labore, magnam libero quam. Illo, quo.' },
-        { title: 'Item one', price: '$24.99', description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veritatis vitae sapiente at assumenda ipsum. Et, deleniti nostrum accusamus commodi alias quaerat distinctio dolorum labore, magnam libero quam. Illo, quo.' },
-        { title: 'Item one', price: '$24.99', description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veritatis vitae sapiente at assumenda ipsum. Et, deleniti nostrum accusamus commodi alias quaerat distinctio dolorum labore, magnam libero quam. Illo, quo.' },
-        { title: 'Item one', price: '$24.99', description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veritatis vitae sapiente at assumenda ipsum. Et, deleniti nostrum accusamus commodi alias quaerat distinctio dolorum labore, magnam libero quam. Illo, quo.' },
-        { title: 'Item one', price: '$24.99', description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi veritatis vitae sapiente at assumenda ipsum. Et, deleniti nostrum accusamus commodi alias quaerat distinctio dolorum labore, magnam libero quam. Illo, quo.' }
-    ]
-    renderItemWithMap = () => {
-        const arrItemJSX = this.items.map((item, index) => {
-            return <div className="col-4" key={index}>
-                <div className="card text-left">
-                    <img className="card-img-top w-100" src="http://placehold.it/700x400" alt />
-                    <div className="card-body">
-                        <h4 className="card-title">{item.title}</h4>
-                        <p className="card-text">{item.price}</p>
-                        <p className="card-text">{item.description}</p>
-                    </div>
-                    <div className="card-footer">
-                        <p>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="fa fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </p>
-                    </div>
-                </div>
-
-            </div>
-        });
-        return arrItemJSX;
-    };
+    state = {
+        imgSrc: './glassesImage/v1.png',
+        title: 'GUCCI G8850U'
+    }
+    changeGlass = (newGlass, newTitle) => {
+        this.setState({
+            imgSrc: `./glassesImage/${newGlass}.png`,
+            title: `${newTitle}`
+        })
+    }
     render() {
         return (
-            <div className="main">
-                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                    <ol className="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active" />
-                        <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-                        <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-                    </ol>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img className="d-block w-100" src="http://placehold.it/900x350" alt="First slide" />
+
+            <div className="container-fluid">
+                <h3 className="title">TRY GLASSES APP ONLINE</h3>
+                <div className="container fashion">
+                    <div class="row">
+                        <div className="col-6">
+                            <div className="fashion">
+                                <img src="./glassesImage/model.jpg" ></img>
+                                <div className="fashion-child1">
+                                    <img src={this.state.imgSrc}></img>
+                                </div>
+                                <div className="fashion-child2">
+                                    <h4>{this.state.title}</h4>
+                                    <p>Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. </p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="http://placehold.it/900x350" alt="Second slide" />
-                        </div>
-                        <div className="carousel-item">
-                            <img className="d-block w-100" src="http://placehold.it/900x350" alt="Third slide" />
+                        <div className="col-6">
+                            <div className="fashion">
+                                <img src="./glassesImage/model.jpg" ></img>
+                            </div>
                         </div>
                     </div>
-                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true" />
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true" />
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
-                <div className="container">
-                    <div class="row">
-                        {this.renderItemWithMap()}
+                    <div class="row glass">
+                        <div className="col-2 text-center">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v1','GUCCI G8850U');
+                                }}
+                                    src="./glassesImage/g1.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2 ">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v2','GUCCI G8759H');
+                                }} src="./glassesImage/g2.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v3','DIOR D6700HQ');
+                                }} src="./glassesImage/g3.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v4','DIOR D6005U');
+                                }} src="./glassesImage/g4.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v5','PRADA P8750');
+                                }} src="./glassesImage/g5.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v6','PRADA P9700');
+                                }} src="./glassesImage/g6.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v7','FENDI F8750');
+                                }} src="./glassesImage/g7.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v8','FENDI F8500');
+                                }}
+                                    src="./glassesImage/g8.jpg" ></img>
+                            </div>
+                        </div>
+                        <div className="col-2">
+                            <div className="glass-item">
+                                <img onClick={() => {
+                                    this.changeGlass('v9','FENDI F4300');
+                                }}
+                                    src="./glassesImage/g9.jpg" ></img>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
