5fa4e6dd0de221177575cb46428e280b4583f2cf
congminhle44 <46514752+congminhle44@users.noreply.github.com>
2020-12-16 17:36:51 +0700

    fix: count up index

--- src/redux/BaiTapQuanLyNguoiDungReducer.js
+++ src/redux/BaiTapQuanLyNguoiDungReducer.js
@@ -1,46 +1,55 @@
+/** @format */
 
 const stateDefault = {
-
-    mangNguoiDung: [
-        { id: 1, taiKhoanKH: 'nguyenvanbinh', hoTen: 'Nguyen van an', matKhau: 123, email: 'nguyenvana@gmail.com', soDienThoai: 123 },
-    ],
-    nguoiDungRedux: {
-        values: {
-            id: 1,
-            taiKhoanKH: '',
-            matKhau: '',
-            hoTen: '',
-            email: '',
-            soDienThoai: ''
-        },
-        errors: {
-            taiKhoanKH: '',
-            matKhau: '',
-            hoTen: '',
-            email: '',
-            soDienThoai: ''
-        }
-    }
-}
+  mangNguoiDung: [
+    {
+      id: 1,
+      taiKhoanKH: 'nguyenvanbinh',
+      hoTen: 'Nguyen van an',
+      matKhau: 123,
+      email: 'nguyenvana@gmail.com',
+      soDienThoai: 123,
+    },
+  ],
+  nguoiDungRedux: {
+    values: {
+      id: 1,
+      taiKhoanKH: '',
+      matKhau: '',
+      hoTen: '',
+      email: '',
+      soDienThoai: '',
+    },
+    errors: {
+      taiKhoanKH: '',
+      matKhau: '',
+      hoTen: '',
+      email: '',
+      soDienThoai: '',
+    },
+  },
+};
 
 export const BaiTapQuanLyNguoiDungReducer = (state = stateDefault, action) => {
-    switch (action.type) {
-        case 'THEM_KHACH_HANG': {
-            const mangUserUpdate=[...state.mangNguoiDung]
-            let index=mangUserUpdate.findIndex(user=>user.id===state.nguoiDungRedux.values.id)
-            if(index !==-1){
-                mangUserUpdate.push(state.nguoiDungRedux.values);
-                index++;
-            }
-            // mangUserUpdate.push(state.nguoiDungRedux.values);
-            // nguoiDungRedux.values.id++;
-            state.mangNguoiDung=mangUserUpdate
-            return{...state}
-        }
-        case 'SET_USER': {
-            state.nguoiDungRedux = action.nguoiDungRedux;
-            return { ...state }
-        }
+  switch (action.type) {
+    case 'THEM_KHACH_HANG': {
+      const mangUserUpdate = [...state.mangNguoiDung];
+      let index = mangUserUpdate.findIndex(
+        (user) => user.id === state.nguoiDungRedux.values.id
+      );
+      if (index !== -1) {
+        mangUserUpdate.push(state.nguoiDungRedux.values);
+      }
+      // mangUserUpdate.push(state.nguoiDungRedux.values);
+      // nguoiDungRedux.values.id++;
+      state.mangNguoiDung = mangUserUpdate;
+      return { ...state };
     }
-    return { ...state }
-}
+    case 'SET_USER': {
+      state.nguoiDungRedux = action.nguoiDungRedux;
+      return { ...state };
+    }
+    default:
+      return { ...state };
+  }
+};
